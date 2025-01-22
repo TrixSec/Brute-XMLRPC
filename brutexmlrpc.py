76eaa73f61e02b9e935ec63087544aed92ecc507
jeninsutradhar <jeninsutradhar@gmail.com>
2025-01-22 17:40:25 +0530

    v2

--- brutexmlrpc.py
+++ brutexmlrpc.py
@@ -17,6 +17,7 @@ from aiohttp_socks import (
     ProxyConnector,
     ProxyType,
 )  # Import the ProxyConnector and ProxyType
+from header_data import user_agents, referer_domains
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 init(autoreset=True)
@@ -43,46 +44,6 @@ def generate_random_ip():
 # ==================================================================================================
 
 def generate_random_headers(target_url):
-    user_agents = [
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.5481.177 Safari/537.36",
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Safari/605.1.15",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0",
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36",
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.3 Mobile/15E148 Safari/604.1",
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64; Trident/7.0; rv:11.0) like Gecko",  # IE 11
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/78.0.3904.108 Chrome/78.0.3904.108 Safari/537.36",  # Chromium
-        "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36 OPR/64.0.3417.92",  # Opera
-        "Mozilla/5.0 (Android 9; Mobile; rv:68.0) Gecko/68.0 Firefox/68.0",  # Mobile Firefox
-        "Mozilla/5.0 (Linux; Android 10; SM-G975F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.90 Mobile Safari/537.36",  # Android Chrome
-        "Mozilla/5.0 (iPad; CPU OS 14_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0 Mobile/15A372 Safari/604.1",  # iPad Safari
-        "Mozilla/5.0 (PlayStation 4 3.11) AppleWebKit/537.73 (KHTML, like Gecko)",  # PlayStation
-        "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",  # Googlebot
-        "Mozilla/5.0 (compatible; bingbot/2.0; +http://www.bing.com/bingbot.htm)",  # Bingbot
-    ]
-
-    referer_domains = [
-        "https://google.com",
-        "https://bing.com", 
-        "https://duckduckgo.com",
-        "https://www.startpage.com/",
-        "https://yahoo.com",
-        "https://baidu.com",
-        "https://yandex.com",
-        "https://ecosia.org",
-        "https://qwant.com",
-        "https://search.brave.com",
-        "https://swisscows.com",
-        "https://metager.org",
-        "https://mojeek.com",
-        "https://gibiru.com",
-        "https://searchencrypt.com",
-        "https://lukol.com",
-        "https://oscobo.com",
-        "https://disconnect.me",
-        "https://peekier.com",
-        "https://boardreader.com"
-    ]
-
     parsed_url = urlparse(target_url)
     if parsed_url.scheme and parsed_url.netloc:
         referer_domains.append(f"{parsed_url.scheme}://{parsed_url.netloc}")
